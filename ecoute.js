1de8787581ce2d553ab9140a899fc76a065b6d63
Gilles FRANCOIS <gilles@sopress.net>
2025-02-21 18:38:16 +0100

    Update ecoute.js

--- ecoute.js
+++ ecoute.js
@@ -3,13 +3,25 @@ ws.onopen = () => console.log("WebSocket online ! Call https://boutsdessai.allwe
 ws.onmessage = (event) => {
     console.log("Message reçu :", event.data);
     const payload = JSON.parse(event.data);
-    const { call } = payload;
+    const { call, message, delay, wait } = payload;
 
+console.log('Message:',message);
     if (!call) return;
 
-    console.log('Calling webhook ', call)
-    fetch(call)
-        .then(response => response.text())
-        .then(data => console.log('Response from webhook:', data))
-        .catch(error => console.error('Error calling webhook:', error));
+    console.log('Calling webhook in '+delay+'s', call)
+	appel(call, wait, delay);
+
+}
+
+function appel (call, wait, delay=0) {
+	setTimeout(() => fetch(call)
+        .then(response => response.json())
+        .then(data => {
+		console.log('Response from webhook:', data)
+		if(wait && data.IsSuccessful === false) {
+			setTimeout(() => appel(call,wait, delay),1000)
+		}
+	})
+
+        .catch(error => console.error('Error calling webhook:', error)), delay*1000);
 }
