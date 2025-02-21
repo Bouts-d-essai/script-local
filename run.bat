3b18c8999d612b021970cd10df6ec5ebb06aaf1e
Gilles FRANCOIS <gilles@sopress.net>
2025-02-21 18:37:59 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,7 +5,9 @@ set "SCRIPT_DIR=%~dp0"
 set "ECOUTE_JS=%SCRIPT_DIR%ecoute.js"
 
 :: Run Nodemon on ecoute.js
-echo 🚀 Running nodemon for ecoute.js...
-nodemon "%ECOUTE_JS%"
 
-pause
+:loop
+echo 🚀 Running nodemon for ecoute.js...
+node "%ECOUTE_JS%"
+
+goto loop
